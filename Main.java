6d87649bd7619d7ac8a456a5ad9b7857085c2a83
yohanes <yohanessitorusp@gmail.com>
2023-12-20 18:38:09 +0700

    edit file Main.java

--- Main.java
+++ Main.java
@@ -3,31 +3,45 @@ import java.util.Random ;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class MosaicTester{
+public class Main{
     public static void main(String[] args){
-        Mosaic mosaic = new Mosaic(gridSize) ;
-        try {
-            sc = new Scanner(new File("input.txt"));
-            int gridSize = sc.nextInt() ;
-            for(int i ; i < gridSize ; i++) {
-                for(int j ; j < gridSize ; j++) {
-                    mosaic.setGrid(i, j, sc.nextInt()) ;
+        Scanner sc = new Scanner(System.in);
+   		int loop = sc.nextInt();
+   		System.out.println("Target: 13692887");
+   		Random init = new Random();
+    	for (int ct=1;ct<=loop;ct++) {
+    		long seed = init.nextLong();
+			System.out.println(seed);
+			Random gen = new Random(seed);
+	    	int mosaicSize=0, totalGeneration=0, maxPopulationSize=0;
+	    	double crossoverRate=0.0, mutationRate=0.0, elitismPct=0.0;
+            Mosaic mosaic ;
+            try {
+                sc = new Scanner(new File("input.txt"));
+                mosaicSize = sc.nextInt() ;
+                mosaic = new Mosaic(mosaicSize) ;
+                for(int i ; i < mosaicSize ; i++) {
+                    for(int j ; j < mosaicSize ; j++) {
+                        mosaic.setGrid(i, j, sc.nextInt()) ;
+                    }
                 }
-            }
-        } catch (FileNotFoundException e) { e.printStackTrace();}
-
-        try {
-            sc = new Scanner(new File("param.txt"));
-            totalGeneration = sc.nextInt();
-            maxPopulationSize = sc.nextInt();
-            crossoverRate = sc.nextDouble();
-            mutationRate = sc.nextDouble();
-            elitismPct = sc.nextDouble();
-        } catch (FileNotFoundException e) { e.printStackTrace();}
-
-        MosaicGA ga = new MosaicGA(gen,totalGeneration, maxPopulationSize, crossoverRate,
-                                        mutationRate, elitismPct, mosaic);
-        
+            } catch (FileNotFoundException e) { e.printStackTrace();}
+    
+            try {
+                sc = new Scanner(new File("param.txt"));
+                totalGeneration = sc.nextInt();
+                maxPopulationSize = sc.nextInt();
+                crossoverRate = sc.nextDouble();
+                mutationRate = sc.nextDouble();
+                elitismPct = sc.nextDouble();
+            } catch (FileNotFoundException e) { e.printStackTrace();}
+    
+            MosaicGA ga = new MosaicGA(gen,totalGeneration, maxPopulationSize, crossoverRate,
+                                            mutationRate, elitismPct, mosaic);
+            Individual res = ga.run();
+        }
+       
     }
 
 }
+
